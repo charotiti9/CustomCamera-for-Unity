10ab3c8642a303da9fa1080c6bdfcfff3cdafa4c
charotiti9 <charotiti9@gmail.com>
2019-07-04 13:53:20 +0900

    Add Move_Keyboard.cs
    Move to default WASD.

---
+++ CustomCamera/Scripts/Feature/Move_Keyboard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Keyboard : CameraSystem
+{
+    [Header("키보드 입력키")]
+    public KeyCode fowardKey = KeyCode.W;
+    public KeyCode LeftKey = KeyCode.A;
+    public KeyCode BackKey = KeyCode.S;
+    public KeyCode RightKey = KeyCode.D;
+    [Header("움직임 속도")]
+    public float moveSpeed = 3f;
+
+    private Transform target;
+
+    /// <summary>
+    /// CameraController에서 실행할 공통된 스타트 부분
+    /// </summary>
+    public override void CommonStart()
+    {
+        target = camManager.target;
+    }
+
+    /// <summary>
+    /// CameraController에서 실행할 공통된 업데이트 부분
+    /// </summary>
+    public override void CommonUpdate()
+    {
+        if (isActive)
+            Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(fowardKey))
+        {
+            target.position += Vector3.forward * 3 * Time.deltaTime;
+        }
+        if (Input.GetKey(LeftKey))
+        {
+            target.position += -Vector3.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(BackKey))
+        {
+            target.position += -Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(RightKey))
+        {
+            target.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
